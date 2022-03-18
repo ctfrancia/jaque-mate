e8a83295b6aa6251cee0b87200fdebc5409ddeb4
Christian Francia <christian.francia@adamo.es>
2022-03-18 14:35:55 +0100

    jsonReader improvemeny

--- cmd/api/tournaments.go
+++ cmd/api/tournaments.go
@@ -15,7 +15,7 @@ func (app *application) createTournamentHandler(w http.ResponseWriter, r *http.R
 	}
 	err := app.readJSON(w, r, &input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
